ec3b6bed784ff40201cbaf3ad762a67c437aa6eb
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-08-07 21:50:26 +0300

    styling

--- src/main/java/com/CodeFellowshipDemo/CodeFellowship/controllers/AppUserController.java
+++ src/main/java/com/CodeFellowshipDemo/CodeFellowship/controllers/AppUserController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,6 +27,17 @@ public class AppUserController {
         List<AppUser> myList = appUserRepo.findAll();
         model.addAttribute("myList", myList);
         return "profile" ;
+    }
+
+    @GetMapping("/user/{userId}")
+    public String getProfilePage(@PathVariable Long userId, Model model){
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        AppUser appUser = appUserRepo.findById(userId).orElseThrow();
+        model.addAttribute("appUser" , appUser);
+        model.addAttribute("posts" , appUser.getPosts());
+        List<AppUser> myList = appUserRepo.findAll();
+        model.addAttribute("myList", myList);
+        return "user" ;
     }
 }
 
